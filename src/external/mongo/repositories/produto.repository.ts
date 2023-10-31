a7632bdd8fc1679dddbd0c0d0ad621b0d2c8a777
fanticheli <igor.fanticheli@hotmail.com>
2023-10-31 19:57:24 -0300

    Testando proteção branch main

--- src/external/mongo/repositories/produto.repository.ts
+++ src/external/mongo/repositories/produto.repository.ts
@@ -7,6 +7,7 @@ import { ProdutoMongo } from "../model/produto";
 export class ProdutoRepositoryInMongo implements IProdutoGateway {
 
 	private _model;
+	private _modelteste;
 
 	constructor() {
 		this._model = ProdutoMongo;
