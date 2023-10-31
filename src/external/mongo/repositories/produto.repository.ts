40e1030f7481cf4b4f783f6319ed3f4f03c8fb14
fanticheli <igor.fanticheli@hotmail.com>
2023-10-31 20:00:54 -0300

    Testando

--- src/external/mongo/repositories/produto.repository.ts
+++ src/external/mongo/repositories/produto.repository.ts
@@ -7,6 +7,7 @@ import { ProdutoMongo } from "../model/produto";
 export class ProdutoRepositoryInMongo implements IProdutoGateway {
 
 	private _model;
+	private _modelTeste;
 
 	constructor() {
 		this._model = ProdutoMongo;
